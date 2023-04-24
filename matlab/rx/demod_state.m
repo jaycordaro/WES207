6ae7035bf871eca1ebc78107c490ef0d0339810d
Sam Weismuller <sampweis@gmail.com>
2023-04-23 23:05:45 -0700

    Rx simulation code cleanup

--- matlab/rx/demod_state.m
+++ matlab/rx/demod_state.m
@@ -2,6 +2,7 @@ function [new_state, new_idx, output_bit] = sfd_state(y, old_idx, symbol_len, po
     output_bit = to_digital(polarity*real(y(old_idx)));
     
     new_state = Rx_State.Demod;
+    
+    % advance to next bit
     new_idx = old_idx + symbol_len;
-   
 end
--- matlab/rx/sfd_state.m
+++ matlab/rx/sfd_state.m
@@ -1,25 +1,30 @@
 function [new_state, new_idx, new_SFD_buff, corr, polarity] = sfd_state(y, old_idx, symbol_len, SFD_polar, SFD_buff, power)
-    new_SFD_buff = [SFD_buff(2:end) y(old_idx)];
     
+    % find correlation with SFD sequence
+    new_SFD_buff = [SFD_buff(2:end) y(old_idx)]; 
     corr = 0;
     for idx = 1:length(SFD_polar)
         corr = corr + SFD_polar(idx)*new_SFD_buff(idx);
     end
     
+    % check if correlation exceeds the threshold
     SFD_thresh = 0.4*length(SFD_polar);
     if abs(corr)/power > SFD_thresh
+        % move to demod state
         new_state = Rx_State.Demod;
+        
+        % determine 0-1 polarity
         if real(corr) > 0
             polarity = 1;
         else
             polarity = -1;
         end
     else
+        % keep searching
         new_state = Rx_State.SFD_Search;
         polarity = 1;
     end
     
+    % advance to next bit
     new_idx = old_idx + symbol_len;
-    
-    
 end
