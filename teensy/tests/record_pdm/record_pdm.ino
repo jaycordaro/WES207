21cc3dc58d71f7ad513e04473b1022353c37149d
Jay Cordaro <jcordaro@ucsd.edu>
2023-06-04 17:28:27 -0700

    fix record_pdm

--- teensy/tests/record_pdm/record_pdm.ino
+++ teensy/tests/record_pdm/record_pdm.ino
@@ -10,6 +10,45 @@ AudioRecordQueue         queue1;         //xy=527.2727203369141,418.272727966308
 AudioConnection          patchCord1(pdm2_1, queue1);
 // GUItool: end automatically generated code
 
+#define MY_ASSERT(cond, message) (my_assert((cond), __LINE__, __FILE__, message, true))
+#define MY_ASSERT_NON_FATAL(cond, message) (my_assert((cond), __LINE__, __FILE__, message, false))
+
+void my_assert(bool cond, int line, const char* file, const char* message, bool fatal)
+{
+  if(!cond)
+  {
+    if(fatal)
+    {
+      while(true)
+      {
+        Serial.printf("Assertion Failed in file %s, line %d: %s\n", file, line, message);
+        delay(10000);
+      }
+    }
+    else
+    {
+      Serial.printf("Assertion Failed in file %s, line %d: %s\n", file, line, message);
+    }
+  }
+}
+
+String getCommand()
+{
+  String command = "";
+  while(command == "")
+  {
+    command = Serial.readString(1024);
+    delay(1);
+  }
+  return command.trim();
+}
+
+int getValue()
+{
+  String cmd = getCommand();
+  return cmd.toInt();
+}
+
 File fout;
 
 const char outfile[] = "testout.bin";
@@ -36,6 +75,7 @@ void setup() {
       fout = SD.open(outfile, FILE_WRITE);
       queue1.clear();
       queue1.begin();
+      break;
     }
   }
   
@@ -68,6 +108,7 @@ void loop() {
         fout = SD.open(outfile, FILE_WRITE);
         queue1.clear();
         queue1.begin();
+        break;
       }
     }
   }
